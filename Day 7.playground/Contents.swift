ae9ce86dd44fdf37f30960ff3c18b0d4d8ce5b51
aalekseenko23 <148762775+aalekseenko23@users.noreply.github.com>
2023-12-06 10:41:28 +0100

    Task one done

--- Day 7.playground/Contents.swift
+++ Day 7.playground/Contents.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 func ifTwoStringAreTheSame(stringOne: String, stringTwo: String) -> Bool {
-    let result = stringOne == stringTwo
+    //sorted() means regardless of the order
+    let result = stringOne.sorted() == stringTwo.sorted()
     return result
 }
 
-ifTwoStringAreTheSame(stringOne: "a", stringTwo: "ab")
+ifTwoStringAreTheSame(stringOne: "ba", stringTwo: "ab")
