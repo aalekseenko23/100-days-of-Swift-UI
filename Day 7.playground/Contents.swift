5692d9d082fdf324e904e031b7f418a27572a4fd
aalekseenko23 <148762775+aalekseenko23@users.noreply.github.com>
2023-12-07 08:51:00 +0100

    Day 8 complete

--- Day 7.playground/Contents.swift
+++ Day 7.playground/Contents.swift
@@ -7,3 +7,11 @@ func ifTwoStringAreTheSame(stringOne: String, stringTwo: String) -> Bool {
 }
 
 ifTwoStringAreTheSame(stringOne: "ba", stringTwo: "ab")
+
+func calculateHypotenuse(side_A: Double, side_B: Double) -> Double {
+
+    return sqrt(side_A * side_B)
+}
+
+calculateHypotenuse(side_A: 7, side_B: 2)
+print(calculateHypotenuse(side_A: 2, side_B: 3))
---
+++ Day 8.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func timeTable (for number: Int, end: Int = 10) {
+    for i in 1...end {
+        print("For \(number) the timestable is \(number * i)")
+    }
+}
+
+timeTable(for: 2)
+timeTable(for: 1 )
